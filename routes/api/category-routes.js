87e9d571644dc336bb2bfba4fc6957ff01cb75f3
Jack Mabie <johnmabie94@gmail.com>
2023-03-16 21:28:19 -0500

    fully fleshing out api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
       include: [{ model: Product }],
-    });;
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
